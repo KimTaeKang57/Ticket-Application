7f9fb774f0f20e99768daeaa7f8cb91be2952075
KimTaeKang57 <zizoen123@gmail.com>
2022-12-06 16:29:19 +0900

    refac #1 : Audience 캡슐화를 위한 Audience, TicketSeller 리팩토링

--- src/main/java/domain/TicketSeller.java
+++ src/main/java/domain/TicketSeller.java
@@ -12,11 +12,6 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        Ticket ticket = ticketOffice.getTicket();
-        if (!audience.getBag().hasInvitation()) {
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-        }
-        audience.getBag().setTicket(ticket);
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
